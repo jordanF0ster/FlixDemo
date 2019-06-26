20fa2cab07c57ca2b32895145b63c6723ab6c06e
Jordan Foster <jordan487@fb.com>
2019-06-26 15:52:22 -0700

    refresh control

---
+++ FlixDemo/Views/MovieCell.h
@@ -0,0 +1,17 @@
+//
+//  MovieCell.h
+//  FlixDemo
+//
+//  Created by jordan487 on 6/26/19.
+//  Copyright © 2019 jordan487. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FlixDemo/Views/MovieCell.m
@@ -0,0 +1,24 @@
+//
+//  MovieCell.m
+//  FlixDemo
+//
+//  Created by jordan487 on 6/26/19.
+//  Copyright © 2019 jordan487. All rights reserved.
+//
+
+#import "MovieCell.h"
+
+@implementation MovieCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
